1422df63229170d19d13a3b5b45849d5f4162ec9
Kiran Gadhavi <kirangadhavi@hotmail.com>
2024-08-26 15:42:43 +0100

    checking yml file

--- src/app/page.test.js
+++ src/app/page.test.js
@@ -4,14 +4,15 @@ import Home from "./page"; // Adjust the import path if necessary
 // import MainComponent from "@/components/Main/MainComponent";
 
 // Mock MainComponent to ensure it is rendered by Home
-jest.mock("@/components/Main/MainComponent", () => () => (
-  <div>MainComponent Mock</div>
-));
+jest.mock("@/components/Main/MainComponent", () => {
+  const MockMainComponent = () => <div>MainComponent Mock</div>;
+  MockMainComponent.displayName = "MainComponent";
+  return MockMainComponent;
+});
 
 describe("Home Component", () => {
   it("renders the MainComponent", () => {
     render(<Home />);
-    // Check if the MainComponent is rendered
     const mainComponent = screen.getByText("MainComponent Mock");
     expect(mainComponent).toBeInTheDocument();
   });
